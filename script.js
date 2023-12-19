14afe187426611ac50f0201415b0aabebe3a8353
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-19 21:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let result="";
+for(let i=1; i<=20; i++){
+	if(i%3==0){
+		result += "Fizz\n";
+	}
+	else if(i%5==0){
+		result += "Buzz\n";
+	}
+	else if(i%3==0 && i%5==0){
+		result += "FizzBuzz\n"
+	}
+	else{
+		result += i +"\n";
+	}
+}
+alert(result);
